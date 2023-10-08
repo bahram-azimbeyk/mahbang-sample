a0b5a304b2090aa7218f677dcec1d457b7a7cf61
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-10-08 19:27:02 +0330

    compelite api service

--- src/app/apis/api.service.ts.service.ts
+++ src/app/apis/api.service.ts.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServiceTsService {
+export class ApiService {
 
-  constructor() { }
+  private baseUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getEmployees() {
+    return this.http.get(`${this.baseUrl}/employee`);
+  }
+
+  getEmployeeById(id: number) {
+    return this.http.get(`${this.baseUrl}/employee/${id}`);
+  }
+
+  createEmployee(data: Partial<Employee>) {
+    return this.http.post(`${this.baseUrl}/create`, data);
+  }
+
+  updateEmployee(id: number, data: Partial<Employee>) {
+    return this.http.put(`${this.baseUrl}/update/${id}`, data);
+  }
+
+  deleteEmployee(id: number) {
+    return this.http.delete(`${this.baseUrl}/delete/${id}`);
+  }
 }
