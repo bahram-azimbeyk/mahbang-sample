d64c8744892fa7d040796089cc08084059448a7e
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-10-08 19:49:44 +0330

    make state file

---
+++ src/app/states/employee-state.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { EmployeeState } from './employee-state.service';
+
+describe('EmployeeState', () => {
+  let service: EmployeeState;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(EmployeeState);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
