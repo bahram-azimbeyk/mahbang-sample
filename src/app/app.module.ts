3c8734ef0f0a57292c1f51277df9e862dba29b1c
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-10-08 19:20:55 +0330

    make employee-dialog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeDialogComponent } from './components/employee-dialog/employee-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    EmployeeDialogComponent
   ],
   imports: [
     BrowserModule,
