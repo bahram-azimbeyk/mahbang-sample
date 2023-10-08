f911c6126d879ccd3409dfc22eb16a64f112ff74
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-10-08 20:09:29 +0330

    updaate facade logic

--- src/app/facade/employee.facade.ts
+++ src/app/facade/employee.facade.ts
@@ -1,31 +1,51 @@
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
 import { ApiService } from '../apis/api.service';
+import { EmployeeState } from '../states/employee-state';
 import { Employee } from '../models/employee.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeFacade {
 
-  constructor(private apiService: ApiService) { }
+  constructor(private api: ApiService, private state: EmployeeState) { }
 
-  getAllEmployees() {
-    return this.apiService.getEmployees();
+  getEmployeesList$(): Observable<Employee[]> {
+    return this.state.getEmployeesList$();
   }
 
-  getEmployeeDetails(id: string) {
-    return this.apiService.getEmployeeById(id);
+  loadAllEmployees(): void {
+    this.api.getEmployees().pipe(
+      map(data => {
+        const employees = data['data'] as Employee[];
+        this.state.setEmployeesList(employees);
+      })
+    );
   }
 
-  addEmployee(data: Partial<Employee>) {
-    return this.apiService.createEmployee(data);
+  createEmployee(data: Partial<Employee>): void {
+    this.api.createEmployee(data).pipe(
+      map(data => {
+        const employee = data['data'] as Employee;
+        this.state.addEmployee(employee);
+      })
+    );
   }
 
-  editEmployee(id: string, data: Partial<Employee>) {
-    return this.apiService.updateEmployee(id, data);
+  editEmployee(id: string, data: any): void {
+    this.api.updateEmployee(id, data).pipe(
+      map(data => {
+        const employee = data['data'] as Employee;
+        this.state.updateEmployee(employee);
+      })
+    );
   }
 
-  removeEmployee(id: string) {
-    return this.apiService.deleteEmployee(id);
+  deleteEmployee(id: string): void {
+    this.api.deleteEmployee(id).subscribe(() => {
+      this.state.deleteEmployee(id);
+    });
   }
 }
