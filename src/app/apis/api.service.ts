5bf20a99c16fbc8340ff6a056dd487b214513ec3
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-10-08 20:09:49 +0330

    define return type for apis

--- src/app/apis/api.service.ts
+++ src/app/apis/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Employee } from '../models/employee.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +13,24 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getEmployees() {
+  getEmployees(): Observable<any> {
     return this.http.get(`${this.baseUrl}/employee`);
   }
 
-  getEmployeeById(id: string) {
+  // unused api!
+  getEmployeeById(id: string): Observable<any> {
     return this.http.get(`${this.baseUrl}/employee/${id}`);
   }
 
-  createEmployee(data: Partial<Employee>) {
+  createEmployee(data: Partial<Employee>): Observable<any> {
     return this.http.post(`${this.baseUrl}/create`, data);
   }
 
-  updateEmployee(id: string, data: Partial<Employee>) {
+  updateEmployee(id: string, data: Partial<Employee>): Observable<any> {
     return this.http.put(`${this.baseUrl}/update/${id}`, data);
   }
 
-  deleteEmployee(id: string) {
+  deleteEmployee(id: string): Observable<any> {
     return this.http.delete(`${this.baseUrl}/delete/${id}`);
   }
 }
