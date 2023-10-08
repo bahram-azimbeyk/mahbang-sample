8d4bab3b14a45613938e5a446c34ca3d4fd13299
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-10-08 19:31:58 +0330

    fill facade layer with apis

--- src/app/facade/employee.facade.ts
+++ src/app/facade/employee.facade.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../apis/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeFacade {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  getAllEmployees() {
+    return this.apiService.getEmployees();
+  }
+
+  getEmployeeDetails(id: number) {
+    return this.apiService.getEmployeeById(id);
+  }
+
+  addEmployee(data: any) {
+    return this.apiService.createEmployee(data);
+  }
+
+  editEmployee(id: number, data: any) {
+    return this.apiService.updateEmployee(id, data);
+  }
+
+  removeEmployee(id: number) {
+    return this.apiService.deleteEmployee(id);
+  }
 }
