de44d1dd8a8167fa64853c89a6ee6e9fc59c9cb2
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-10-08 19:31:31 +0330

    update api service

--- src/app/apis/api.service.spec.ts
+++ src/app/apis/api.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiServiceTsService } from './api.service.ts.service';
+import { ApiService } from './api.service';
 
-describe('ApiServiceTsService', () => {
-  let service: ApiServiceTsService;
+describe('ApiService', () => {
+  let service: ApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiServiceTsService);
+    service = TestBed.inject(ApiService);
   });
 
   it('should be created', () => {
