8d58aa499f5733e53180a815d2c09be20e05e69d
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-10-09 11:11:34 +0330

    write test for dialog

--- src/app/components/employee-dialog/employee-dialog.component.spec.ts
+++ src/app/components/employee-dialog/employee-dialog.component.spec.ts
@@ -1,15 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { EmployeeDialogComponent } from './employee-dialog.component';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('EmployeeDialogComponent', () => {
+fdescribe('EmployeeDialogComponent', () => {
   let component: EmployeeDialogComponent;
   let fixture: ComponentFixture<EmployeeDialogComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [EmployeeDialogComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
+    })
+      .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [EmployeeDialogComponent]
-    });
     fixture = TestBed.createComponent(EmployeeDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +35,34 @@ describe('EmployeeDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the form', () => {
+    expect(component.form).toBeDefined();
+    expect(component.form.get('name')).toBeDefined();
+    expect(component.form.get('salary')).toBeDefined();
+    expect(component.form.get('age')).toBeDefined();
+  });
+
+  it('should validate the form', () => {
+    const nameControl = component.form.get('name');
+    const salaryControl = component.form.get('salary');
+    const ageControl = component.form.get('age');
+
+    // Initially, all fields are invalid
+    expect(nameControl?.valid).toBeFalsy();
+    expect(salaryControl?.valid).toBeFalsy();
+    expect(ageControl?.valid).toBeFalsy();
+
+    // Set values
+    nameControl?.setValue('John Doe');
+    salaryControl?.setValue('50000');
+    ageControl?.setValue('30');
+
+    // Now, all fields should be valid
+    expect(nameControl?.valid).toBeTruthy();
+    expect(salaryControl?.valid).toBeTruthy();
+    expect(ageControl?.valid).toBeTruthy();
+  });
+
+  // You can add more tests for form submission, dialog actions, etc.
 });
